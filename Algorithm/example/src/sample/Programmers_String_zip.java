7d4194c5388e366228449dbf25cc9ab8dd6eec94
dongkii <31609765+dongkii@users.noreply.github.com>
2022-01-22 23:25:45 +0900

    q2 solution update

--- Algorithm/example/src/sample/Programmers_String_zip.java
+++ Algorithm/example/src/sample/Programmers_String_zip.java
@@ -3,58 +3,48 @@ package sample;
 import java.util.*;
 
 public class Programmers_String_zip {
+
 	public static int solution(String s) {
-		System.out.println("입력값 : \"" + s + "\" 문자열 길이 : " + s.length());
+		int[] len = new int[s.length()];	// 각 문자열을 i개로 잘랐을 때 문자열 길이를 체크하기 위한 배열
 		
-		List<Integer> list = new ArrayList<Integer>(); 
-		
-		
-        for(int i = 1; i < s.length()+1; i++) {
-        	int share = s.length()/i;
-        	int mod = s.length()%i;
-        	
-        	Stack<String> st = new Stack<String>();
-        	String result = "";
-        	
-        	if(mod == 0) {
-        		String prevStr = ""; 	// 값 비교를 위한 스트링
-        		int j = 0;
-        		while(j < share) {
-        			int tp = j+i;
-        			String strTmp = s.substring(j, tp);
-        			if(st.size() == 0 || prevStr.equals(strTmp)) {
-        				st.push(strTmp);
-        			} else {
-        				if(st.size() == 1) {
-        					result += st.pop();
-        				} else {
-        					result += st.size() + st.pop();
-        				}
-        				
-        				st.clear();
-        				st.push(strTmp);
-        			}
-        			prevStr = strTmp;
-        		}
+        for(int i = 1; i < s.length()+1; i++) {        	
+        	String result = "";		// 압축된 문자열을 저장하기 위한 변수
+			String prevStr = ""; 	// 값 비교를 위한 스트링
+        	int cnt = 0;
+
+        	for(int j = 0; j < s.length(); j+=i) {
+                String strTmp = "";
+                if(j+i > s.length()) {			// 서브스트링의 EndIndex가 문자열의 길이를 넘어가지 않도록
+                    strTmp = s.substring(j);	// j 부터 마지막 인덱스까지
+				} else {
+                    strTmp = s.substring(j, j+i);	// j 부터 j+i 까지
+                }
         		
-    		    if(st.size() == 1) {
-    				result += st.pop();
-    			} else {
-    				result += st.size() + st.pop();
-    			}
-    	        
-            	list.add(result.length());
-            	System.out.println(i + "개로 자를 때 : " + result);
+        		if(cnt == 0 || prevStr.equals(strTmp)) {	// 이전값과 비교해서 이전값과 같으면 cnt 값 증가
+                   cnt++;
+        		} else {	// 이전 서브스트링 값과 다르면
+        			if(cnt == 1) {	// 1은 생략
+        				result += prevStr;
+        			} else {		// 압축된 문자열 갯수 + 압축된 문자열
+        				result += cnt + prevStr;
+        			}
+        			cnt = 1;	// result에 이전 데이터 값이 저장되니 cnt는 1로 초기화
+        		}
+        		prevStr = strTmp;
         	}
-        	
-        	
-        }
+                
+            if(cnt == 1) {		// 위에서 마지막 for문이 실행된 후 result에 저장되지 못한 문자열 저장
+                result += prevStr;
+			} else {
+            	result += cnt + prevStr;
+            }
+    	        
+            len[i-1] = result.length();		// len 배열에 result 문자열의 길이 저장
+        }        
         
-        Arrays.sort(list.toArray());
+        Arrays.sort(len);		// 압축된 문자열중 가장 짧은 길이를 가져오기위해 sort 후 0번째 인덱스(오름차순이라 가장작은값이 0번째에)
         
-		int answer = list.get(0);
-        
-        System.out.println("기댓값 : " + answer);
+		int answer = len[0];
         return answer;
     }
 	
